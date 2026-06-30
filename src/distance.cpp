7a4518783831738492f9b2787ccf895a8482eaf8
saimaakter885 <saimaakter@ufl.edu>
2026-06-30 17:04:55 -0400

    added distance.h and distance.cpp

--- src/distance.cpp
+++ src/distance.cpp
@@ -1,1 +1,22 @@
+#include "distance.h"
+#include <cmath>
+
+double haversineDistance(double lat1, double lon1, double lat2, double lon2){
+    const double earth_r = 6371000.0;
+
+    double rad_lat1 = lat1 * M_PI / 180.0;
+    double rad_lat2 = lat2 * M_PI / 180.0;
+    double rad_lon1 = lon1 * M_PI / 180.0;
+    double rad_lon2 = lon2 * M_PI / 180.0;
+
+    double deltaLat = rad_lat2 - rad_lat1;
+    double deltaLon = rad_lon2 - rad_lon1;
+
+    double latLonDifference = sin(deltaLat / 2) * sin(deltaLat / 2) + cos(rad_lat1) * cos(rad_lat2) *
+               sin(deltaLon / 2) * sin(deltaLon / 2);
+
+    double angle_rad = 2 * atan2(sqrt(latLonDifference), sqrt(1 - latLonDifference));
+
+    return earth_r * angle_rad;
+}
 
--- src/distance.h
+++ src/distance.h
@@ -1,8 +1,6 @@
-//
-// Created by saima on 6/28/2026.
-//
-
 #ifndef GAINESVILLE_ROAD_NAVIGATOR_DISTANCE_H
 #define GAINESVILLE_ROAD_NAVIGATOR_DISTANCE_H
+
+double haversineDistance(double lat1, double lon1, double lat2, double lon2);
 
 #endif //GAINESVILLE_ROAD_NAVIGATOR_DISTANCE_H
