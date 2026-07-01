ae759ff82ea37e398fc08956a145f903ab495753
saimaakter885 <saimaakter@ufl.edu>
2026-07-01 13:56:35 -0400

    added graph.cpp

--- src/graph.cpp
+++ src/graph.cpp
@@ -1,3 +1,57 @@
-//
-// Created by saima on 6/28/2026.
-//
+#include "graph.h"
+#include <iostream>
+
+void Graph::addNode(long long id, double lat, double lon)
+{
+    if (nodes.find(id) == nodes.end())
+    {
+        Node newNode;
+        newNode.id = id;
+        newNode.lat = lat;
+        newNode.lon = lon;
+        nodes[id] = newNode;
+    }
+}
+
+Node* Graph::getNode(long long id)
+{
+    if (nodes.find(id) == nodes.end())
+    {
+        return nullptr;
+    }
+    return &nodes[id];
+}
+
+void Graph::addEdge(long long StartId, long long endId, double weight)
+{
+    if (nodes.find(StartId) == nodes.end() || nodes.find(endId) == nodes.end())
+    {
+        return;
+    }
+    Node* startNode = getNode(StartId);
+    Node* endNode = getNode(endId);
+
+    Edge forwardEdge;
+    forwardEdge.destination = endNode;
+    forwardEdge.weight = weight;
+    startNode->edges.push_back(forwardEdge);
+
+    Edge backwardEdge;
+    backwardEdge.destination = startNode;
+    backwardEdge.weight = weight;
+    endNode->edges.push_back(backwardEdge);
+
+}
+
+void Graph::printGraphInfo()
+{
+    int totalEdge = 0;
+    for (unordered_map<long long, Node>::iterator it = nodes.begin(); it != nodes.end(); it++)
+    {
+        totalEdge += it->second.edges.size();
+    }
+    cout << "Total Nodes: " << nodes.size() << endl;
+    cout << "Total Edges: " << totalEdge / 2 << endl;
+}
+
+
