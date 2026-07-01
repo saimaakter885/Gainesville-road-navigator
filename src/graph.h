509917c05dc3ef29d401219fb072a20e0c7615ac
saimaakter885 <saimaakter@ufl.edu>
2026-06-30 20:01:21 -0400

    added graph.h

--- src/graph.h
+++ src/graph.h
@@ -1,8 +1,35 @@
-//
-// Created by saima on 6/28/2026.
-//
-
 #ifndef GAINESVILLE_ROAD_NAVIGATOR_GRAPH_H
 #define GAINESVILLE_ROAD_NAVIGATOR_GRAPH_H
+
+#include<vector>
+#include<unordered_map>
+using namespace std;
+
+
+struct Node;
+
+struct Edge
+{
+    Node* destination;
+    double weight;
+};
+
+struct Node
+{
+    long long id;
+    double lat;
+    double lon;
+    vector<Edge> edges;
+};
+
+class Graph
+{
+public:
+    unordered_map<long long, Node> nodes;
+    void addNode(long long id, double lat, double lon);
+    void addEdge(long long StartId, long long endId, double weight);
+    Node* getNode(long long id);
+    void printGraphInfo();
+};
 
 #endif //GAINESVILLE_ROAD_NAVIGATOR_GRAPH_H
